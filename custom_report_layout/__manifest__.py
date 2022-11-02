0618871591ccab72a7f398918dc295ad565bc9fd
Tarek Ibrahim <tibrahim@bts.com.eg>
2022-11-02 11:34:06 +0200

    Fix module version to 15

--- custom_report_layout/__manifest__.py
+++ custom_report_layout/__manifest__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Custom Report Layout",
-    'version': '13.0.1.0.0',
+    'version': '15.0.1.0.0',
     'summary': 'A module to configure report header and footer',
-    'sequence': -100,
     'description': 'A module to configure report header and footer manually from company form',
     'author': "Ismail Mohamedi",
     'website': "https://www.linkedin.com/in/ismail-mohamedi-605537179",
