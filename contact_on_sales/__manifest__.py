5cdc227062417fc1081cf71840b638094809aaf3
AbdulrahmanQasem <46605082+AbdulrahmanQasem@users.noreply.github.com>
2022-11-21 11:03:45 +0300

    contact_on_sales

---
+++ contact_on_sales/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    
+    "name": "Customers Contact",
+    "version": "15.0.1.0.1",
+    "author": "ABDULRAHMAN's FOURI",
+    "website": "http://www.fouri.sa",
+    "depends": ["sale_management",'base'],
+    "data" : ["views/contact_on_sales_view.xml"],
+    "installable": True,
+    "auto_install": False,
+}
