d303afaed8e8b39695cb7b59bff8daf12ac74e3e
AbdulrahmanQasem <46605082+AbdulrahmanQasem@users.noreply.github.com>
2022-12-11 15:24:19 +0300

    html_note

---
+++ sh_so_html_notes/README.md
@@ -0,0 +1,27 @@
+About
+============
+This module used to write HTML notes into the sale order/quotation. You can print anything in notes like tables, specification, Terms and condition with bullets points and many more. You can also attach files or images in an HTML note. You can print an HTML note in the report.
+
+User Guide
+============
+Blog: https://www.softhealer.com/blog/odoo-2/post/sale-order-html-notes-492
+Product: /shop/product/sale-order-html-notes-498
+
+Installation
+============
+1) Copy module files to addon folder.
+2) Restart odoo service (sudo service odoo-server restart).
+3) Go to your odoo instance and open apps (make sure to activate debug mode).
+4) click on update app list.
+5) search module name and hit install button.
+
+Any Problem with module?
+=====================================
+Please create your ticket here https://softhealer.com/support
+
+Softhealer Technologies Doubt/Inquiry/Sales/Customization Team
+=====================================
+Skype: live:softhealertechnologies
+What's app: +917984575681
+E-Mail: support@softhealer.com
+Website: https://softhealer.com
---
+++ sh_so_html_notes/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name" : "Sale Order HTML Notes",
+    "author" : "Softhealer Technologies",
+    "website": "https://www.softhealer.com",
+    "support": "support@softhealer.com",        
+    "category": "Extra Tools",
+    "summary": "Sale Order HTML Notes ,Create Sales HTML Note, Generate SO HTML Note With Image,Create, Quotation HTML Note With Attachment,Print Sales HTML Note In Report,HTML NotesWith Terms & Condition,Sale HTML Notes Odoo",
+    "description": """
+        This module used to write HTML notes into the sale order/quotation. You can print anything in notes like tables, specification, Terms and condition with bullets points and many more. You can also attach files or images in an HTML note. You can print an HTML note in the report.
+ Sale Order HTML Notes Odoo
+Create Sales HTML Note, Make Sale Order HTML Notes Module, Generate SO HTML Note With Image, Create Quotation HTML Note With Attachment, Print Sales HTML Note In Report, Make HTML NotesWith Terms & Condition Odoo
+ Create Sales HTML Note App, Make Sale Order Notes Module, Generate SO Note With Image, Create Quotation HTML Note, Print Sales HTML Note, Terms & Condition HTML Notes Odoo
+Orden de venta HTML Notas Odoo
+Crear nota HTML de ventas, módulo de notas HTML de orden de venta, generar nota HTML SO con imagen, crear nota HTML de cotización con archivo adjunto, imprimir nota HTML de ventas en informe, hacer notas HTML con términos y condiciones Odoo
+ Crear aplicación de notas HTML de ventas, crear módulo de notas de pedidos de venta, generar notas SO con imágenes, crear notas HTML de citas, imprimir notas HTML de ventas, términos y condiciones Notas HTML Odoo
+                    """,     
+    "version":"13.0.3",
+    "depends" : ["sale_management"],
+    "application" : True,
+    "data" : [
+        'views/sale_order_views.xml',
+        'report/sale_report.xml',        
+            ],            
+    "images": ["static/description/background.png",],              
+    "auto_install":False,
+    "installable" : True,  
+    "price": 15,
+    "currency": "EUR"      
+}
