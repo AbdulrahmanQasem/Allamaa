304669e4639dc08e90111d3e8ace995dfad27a4f
AbdulrahmanQasem <46605082+AbdulrahmanQasem@users.noreply.github.com>
2022-12-31 13:30:28 +0300

    delete_sales_product_image_model

--- sales_report_product_image/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-from . import sale_product
-from . import product_template
-from . import config_settings
-from . import mrp_production
-
