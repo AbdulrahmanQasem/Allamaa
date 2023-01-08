b62e061a3dd6d798f095857c9e6f9919a5b99b27
AbdulrahmanQasem <46605082+AbdulrahmanQasem@users.noreply.github.com>
2023-01-08 09:37:14 +0300

    Update __manifest__.py

--- asset_qrcode/__manifest__.py
+++ asset_qrcode/__manifest__.py
@@ -2,7 +2,8 @@
 
 {
     'name': 'Assets QR Code',
-    'version': '14.0.2.0.0',
+    "version": "15.0.1.0.0",
+    "license": "LGPL-3",
     'summary': 'Print QR Code Assets.',
     'category': 'Uncategorized',
     'author': '',
